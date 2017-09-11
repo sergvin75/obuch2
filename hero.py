c652d55f870ae7ef625d97d529ce3d355b953962
sergvin75 <jam.program@gmail.com>
2017-09-11 10:32:15 +0300

    словари

---
+++ hero.py
@@ -0,0 +1,24 @@
+class Hero():
+    """Class to Create Hero for our Game"""
+    def __init__(self, name, level,race):
+        """Initiate our hero"""
+        self.name = name
+        self.level = level
+        self.race = race
+        self.health = 100
+
+    def show_hero(self):
+        """Print all parameters of Hero"""
+        discription = (self.name + " Level is " + str(self.level) + " Race is " + self.race + " Health is " + str(self.health).title())
+        print(discription)
+
+    def level_up(self):
+        """Upgrade Leve of Hero"""
+        self.level +=1
+
+    def move(self):
+        """Start moving hero"""
+        print("Hero " + self.name + " start moving...")
+
+    def set_health(self, new_health):
+        self.health = new_health
